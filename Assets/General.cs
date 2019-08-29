47d1349fa5d6b68bf00e9ec8312026b012138987
Alexis Cano <id.alexcan@gmail.com>
2019-08-29 12:00:41 -0500

    Initial commit

---
+++ Assets/General.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class General : MonoBehaviour
+{
+    
+    void Start()
+    {
+        GameObject copito = GameObject.CreatePrimitive(PrimitiveType.Cube) as GameObject;
+        copito.AddComponent<Alpinito>();
+
+        
+    }
+    
+    public virtual void Print()
+    {
+        Debug.Log("capuccino rossini");
+        
+    }
+    
+}
+
+public class Alpinito : General
+{
+    private void Start()
+    {
+        int c = 4;
+        
+    }
+    public override void Print()
+    {
+        Debug.Log("capuccino rossini bambini paganini peperoni");
+
+    }
+
+    
+
+}
+
