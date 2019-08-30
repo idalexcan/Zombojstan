3efdadd738dea56b278b31b9721494de1b786c0b
Alexis Cano <id.alexcan@gmail.com>
2019-08-30 12:18:50 -0500

    Zombojstan

--- Assets/General.cs
+++ Assets/General.cs
@@ -1,27 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class General : MonoBehaviour
 {
-    GameObject person;
-    GameObject yo;
+    const int max= 25;
+    readonly int min;
+    public int cantBody;
+    List<GameObject> bodies = new List<GameObject>();
+
+    public General()
+    {
+        min = Rand.rand.Next(5,15);//(int)Rnd.Basic(5, 15)
+        cantBody = Rand.rand.Next(min, max);
+        
+    }
+
     private void Start()
     {
-        person = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        person.AddComponent<Humanoide.Person>();
-        person.AddComponent<Rigidbody>();
-        person.GetComponent<MeshRenderer>().material.color = Color.cyan;
-
-        yo = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        yo.transform.position = new Vector3(2, 0, 0);
-        yo.AddComponent<Humanoide.Yo>();
-        yo.AddComponent<Rigidbody>();
-        yo.GetComponent<MeshRenderer>().material.color = Color.black;
-
-        int f = 4;
-        f.ToString();
-        
+        for (int i = 0; i < cantBody; i++)
+        {
+            if (Rand.rand.Next(0,2)==0)
+            {
+                Debug.Log(i + ". esto es un zombie");
+            }
+            else
+            {
+                Debug.Log(i + ". esto es un aldeano");
+            }
+        }
     }
 
     private void Update()
@@ -29,37 +37,16 @@ public class General : MonoBehaviour
         
     }
 }
-public class Humanoide : MonoBehaviour
+
+public class Rand // CLASE DE LOS BRANDONS
 {
-    public int life;
-    public string name;
-
-    public class Person : Humanoide
-    {
-        public  bool oncol = false;
-        private void Update()
-        {
-
-            transform.Rotate(0, 0.7f, 0);
-        }
-        //private void OnCollisionEnter(Collision collision)
-        //{
-        //    oncol = true;
-        //}
-    }
-    public class Yo : Humanoide
-    {
-        float speed = 5;
-        private void Update()
-        {
-            if (Input.GetKey("w")) { transform.position += transform.forward * (speed / 20); }
-            if (Input.GetKey("s")) { transform.position -= transform.forward * (speed / 20); }
-            if (Input.GetKey("d")) { transform.position += transform.right * (speed / 20); }
-            if (Input.GetKey("a")) { transform.position -= transform.right * (speed / 20); }
-        }
-        
-    }
+    public static System.Random rand = new System.Random();
 }
+
+
+
+
+
 
 
 
